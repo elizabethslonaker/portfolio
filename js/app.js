ac07d9673c9362b47c7bef3df20e731c62607163
Elizabeth Slonaker <54694151+elizabethslonaker@users.noreply.github.com>
2020-02-02 20:07:58 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,28 +1,17 @@
-// toggle light & dark modes
-var checkbox = document.querySelector('input[name=theme]');
-
-checkbox.addEventListener('change', function() {
-    if(this.checked) {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'dark')
-    } else {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'light')
-    }
-})
-
-let trans = () => {
-    document.documentElement.classList.add('transition');
-    window.setTimeout(() => {
-        document.documentElement.classList.remove('transition')
-    }, 1000)
-}
-
-
-
 // When hamburger menu is clicked, nav bar toggles open
-$(document).ready(function(){
+
+/* $(document).ready(function(){
   $(".hamburgerMenu").click(function(){
     $("nav").slideToggle(500);
   })
-})
+})*/
+
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
