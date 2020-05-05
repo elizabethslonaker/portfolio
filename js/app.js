74591f3e3069ac53a7aa81264cf910ca86343631
Elizabeth Slonaker <54694151+elizabethslonaker@users.noreply.github.com>
2020-05-05 01:29:40 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,17 +1,45 @@
-// When hamburger menu is clicked, nav bar toggles open
+/* Filtering projects on index from w3schools.com */
 
-/* $(document).ready(function(){
-  $(".hamburgerMenu").click(function(){
-    $("nav").slideToggle(500);
-  })
-})*/
-
-
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
+//filterSelection("all")
+filterSelection("caseStudy")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  //if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "showFilter");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "showFilter");
   }
+}
+
+function addClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnFilterContainer = document.getElementById("btnFilterContainer");
+var btns = btnFilterContainer.getElementsByClassName("btn-filter");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("activeFilter");
+    current[0].className = current[0].className.replace(" activeFilter", "");
+    this.className += " activeFilter";
+  });
 }
